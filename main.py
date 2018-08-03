7cebf13a7ad22c682e1a79a9a78aee88e3903969
jiakun <327606424@qq.com>
2018-08-03 10:21:24 +0800

    init

---
+++ main.py
@@ -0,0 +1,11 @@
+#-*- coding:utf-8 -*-
+from flask import Flask
+ 
+app = Flask(__name__, static_url_path='')
+ 
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
